24213f468c3f9fa17d067a88313af76607df00df
Baran ATLIHAN <baranatlihan9@gmail.com>
2022-01-22 13:39:25 +0300

    ligths off when break, seats can breakable
    ligths off when break, seats can breakable

--- Assets/Scripts/EnveriomentScripts/BreakableObject.cs
+++ Assets/Scripts/EnveriomentScripts/BreakableObject.cs
@@ -5,17 +5,49 @@ using UnityEngine;
 public class BreakableObject : MonoBehaviour
 {
 
+    [Header("Lights Settings")]
+    public bool lightsOut = false;
+    public GameObject lightObj;
+
+    [Header("Explode Settings")]
+    public bool explode = false;
+    public Transform[] explodeObj;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             transform.GetChild(1).gameObject.SetActive(false);
             transform.GetChild(0).gameObject.SetActive(true);
+
+            if (explode)
+            {
+                ExplodeEffect();
+            }
+                
+
+
+            if (lightsOut)
+            {
+                lightObj.transform.gameObject.SetActive(false);
+            }
+
             Destroy(this.gameObject, 5);
         }
 
 
     }
+
+
+    void ExplodeEffect()
+    {
+        for (int i = 0; i < explodeObj.Length; i++)
+        {
+            explodeObj[i].GetComponent<Rigidbody>().AddForce(0, 7f, 0, ForceMode.Impulse);
+        }
+    }
+
+    
 }
 
 /*
