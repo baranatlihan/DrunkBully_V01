5fc10c5909a2220c53e331be62ee006ac3b26b6c
Baran ATLIHAN <baranatlihan9@gmail.com>
2022-01-23 14:03:58 +0300

    All breakable objects done.
    breakable objects imporved

--- Assets/Scripts/EnveriomentScripts/BreakableObject.cs
+++ Assets/Scripts/EnveriomentScripts/BreakableObject.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class BreakableObject : MonoBehaviour
 {
+    [HideInInspector]
+    public int numb = 3;
+
+    System.Random random = new System.Random();
 
     [Header("Lights Settings")]
     public bool lightsOut = false;
     public GameObject lightObj;
 
     [Header("Explode Settings")]
+    public bool randomExplode = false;
     public bool explode = false;
     public Transform[] explodeObj;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            numb = random.Next(0, 5);
             transform.GetChild(1).gameObject.SetActive(false);
             transform.GetChild(0).gameObject.SetActive(true);
 
@@ -24,14 +31,18 @@ public class BreakableObject : MonoBehaviour
             {
                 ExplodeEffect();
             }
-                
 
+            if (randomExplode)
+            {
+                RandomExplodeEffect();
+            }
 
             if (lightsOut)
             {
                 lightObj.transform.gameObject.SetActive(false);
             }
 
+            this.GetComponent<Collider>().enabled = false;
             Destroy(this.gameObject, 5);
         }
 
@@ -43,8 +54,22 @@ public class BreakableObject : MonoBehaviour
     {
         for (int i = 0; i < explodeObj.Length; i++)
         {
-            explodeObj[i].GetComponent<Rigidbody>().AddForce(0, 7f, 0, ForceMode.Impulse);
+            explodeObj[i].GetComponent<Rigidbody>().AddForce(0, 6f, 0, ForceMode.Impulse);
         }
+    }
+
+    void RandomExplodeEffect()
+    {
+        for(int i = 0; i < explodeObj.Length; i++)
+        {
+            if(random.Next(0, 5) < numb)
+                explodeObj[i].GetComponent<Rigidbody>().AddForce(2f, 2.5f, -1f, ForceMode.Impulse);
+            else
+                explodeObj[i].GetComponent<Rigidbody>().AddForce(-1.5f, 1.75f, 2f, ForceMode.Impulse);
+
+        }
+        
+
     }
 
     
